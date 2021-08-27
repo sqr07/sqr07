351ebf970b1addc2e88bd9da0f73903a2380080f
sqr07 <89581504+sqr07@users.noreply.github.com>
2021-08-27 11:52:18 +0300

    Add files via upload
    pkg install git

---
+++ sqr07.sh
@@ -0,0 +1,36 @@
+
+clear 
+cd 
+ls
+termux-setup-storage 
+pkg update
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install ruby -y
+pkg install perl -y
+pkg install php -y
+pkg install bash -y
+pkg install nano -y
+pkg install wget -y
+pkg install curl -y
+pkg install nmap -y
+pip install lolcat -y
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+pkg install cmatrix -y
+pkg install proot -y
+pkg install chroot -y
+termux-chroot -y
+pkg install figlet -y
+apt install figlet -y
+apt update 
+apt upgrade 
+clear 
+echo "==================================================="
+figlet [sqr07]
+echo "==================================================="
+cd 
+ls 
+
